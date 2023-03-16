f810749b2be5f2ecb8f9a13ca2c8b400f95d2509
Alexander-1703 <gladyshev.aa@edu.spbstu.ru>
2023-03-16 10:11:29 +0300

    initial commit

---
+++ src/test/java/LoadablePage.java
@@ -0,0 +1,3 @@
+public interface LoadablePage {
+    public void checkPage();
+}
---
+++ src/test/java/LoginPageTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.ui.LoadableComponent;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
+public class LoginPageTest implements LoadablePage {
+    @Test
+    public void loginTest() {
+
+    }
+
+    @Override
+    public void checkPage() {
+
+    }
+}
